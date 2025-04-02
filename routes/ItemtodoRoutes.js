d544c7b22f202f25697d428ca259a5f59b779efc
vaishnavratheesh <161823698+vaishnavratheesh@users.noreply.github.com>
2025-04-02 11:49:06 +0530

    Update ItemtodoRoutes.js

--- routes/ItemtodoRoutes.js
+++ routes/ItemtodoRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Item = require("../models/itemtodo");
+const Item = require("../models/Itemtodo");
 
 
 router.post("/", async (req, res) => {
